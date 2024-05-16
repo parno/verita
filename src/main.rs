e7d177f613abd95f0a46cbaab42574e417de64a9
Bryan Parno <parno@cmu.edu>
2024-05-16 11:27:47 -0400

    Generalize

--- src/main.rs
+++ src/main.rs
@@ -22,52 +22,22 @@ struct Args {
     debug_level: u8,
 }
 
-fn get_z3_version(verus_repo: &PathBuf) -> anyhow::Result<String> {
+fn get_solver_version(verus_repo: &PathBuf, solver_exe: &str, fmt_str: &str) -> anyhow::Result<String> {
     let sh = Shell::new()?;
-    let output = cmd!(sh, "{verus_repo}/source/z3 --version") //.quiet().run()?;
+    let output = cmd!(sh, "{verus_repo}/source/{solver_exe} --version") //.quiet().run()?;
         .output()?;
     //dbg!(&output);
     let output_str = String::from_utf8(output.stdout)?;
-    let v = Regex::new(r"^Z3 version ([0-9.]*) ")?
+    let fmt = format!("{fmt_str} ([0-9.]*) ");
+    let v = Regex::new(&fmt)?
         .captures(&output_str)
-        .ok_or_else(|| anyhow!("Failed to find Z3 version"))?
+        .ok_or_else(|| anyhow!("Failed to find {solver_exe} version"))?
         .get(1)
         .expect("missing capture group")
         .as_str()
         .to_string();
-    println!("Found Z3 version: {v}");
+    println!("Found {solver_exe} version: {v}");
     Ok(v)
-}
-
-fn get_cvc5_version(verus_repo: &PathBuf) -> anyhow::Result<String> {
-    let sh = Shell::new()?;
-    let output = cmd!(sh, "{verus_repo}/source/cvc5 --version") //.quiet().run()?;
-        .output()?;
-    //dbg!(&output);
-    let output_str = String::from_utf8(output.stdout)?;
-    let v = Regex::new(r"^This is cvc5 version ([0-9.]*) ")?
-        .captures(&output_str)
-        .ok_or_else(|| anyhow!("Failed to find cvc5 version"))?
-        .get(1)
-        .expect("missing capture group")
-        .as_str()
-        .to_string();
-    println!("Found cvc5 version: {v}");
-    Ok(v)
-}
-
-fn run(run_configuration_path: &PathBuf) -> Result<(), String> {
-    let run_configuration: RunConfiguration = toml::from_str(
-        &std::fs::read_to_string(run_configuration_path).map_err(|e| {
-            format!(
-                "cannot read configuration file {}: {}",
-                run_configuration_path.display(),
-                e
-            )
-        })?,
-    )
-    .map_err(|e| format!("cannot parse run configuration: {}", e))?;
-    Ok(())
 }
 
 fn main() {
@@ -86,6 +56,6 @@ fn main() {
         .init();
 
     println!("Hello, world!");
-    let _z3_version = get_z3_version(&args.verus_repo);
-    let _cvc_version = get_cvc5_version(&args.verus_repo);
+    let _z3_version = get_solver_version(&args.verus_repo, "z3", "Z3 version");
+    let _cvc5_version = get_solver_version(&args.verus_repo, "cvc5", "This is cvc5 version");
 }
