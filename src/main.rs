0d011f924918247fe4d618cb4390916d0de22a64
Bryan Parno <parno@cmu.edu>
2024-05-16 10:06:53 -0400

    Add some command-line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use std::path::PathBuf;
+use clap::Parser as ClapParser;
+use tracing::{error, info}; // debug, trace
+
+#[derive(ClapParser)]
+#[command(version, about)]
+struct Args {
+    /// Base of the Verus repository
+    #[arg(short, long)]
+    verus_repo: PathBuf,
+
+    /// Path to a run configuration file
+    config: PathBuf,
+
+    /// Print debugging output (can be repeated for more detail)
+    #[arg(short = 'd', long = "debug", action = clap::ArgAction::Count)]
+    debug_level: u8,
+}
+
 fn main() {
+    let args = Args::parse();
+
+    tracing_subscriber::fmt()
+        .with_timer(tracing_subscriber::fmt::time::uptime())
+        .with_level(true)
+        .with_target(false)
+        .with_max_level(match args.debug_level {
+            0 => tracing::Level::WARN,
+            1 => tracing::Level::INFO,
+            2 => tracing::Level::DEBUG,
+            _ => tracing::Level::TRACE,
+        })
+        .init();
+
     println!("Hello, world!");
 }
