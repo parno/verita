8661d11336ec523491cb3fe8e9e681aceb61d38b
Bryan Parno <parno@cmu.edu>
2024-05-17 12:02:24 -0400

    Function-level SMT run times

--- scripts/analyze.py
+++ scripts/analyze.py
@@ -4,14 +4,28 @@ import argparse
 import json
 import glob
 
+class FunctionSmtTime:
+    def __init__(self, json):
+        self.name = json["function"]
+        self.time_ms = json["time"]
+
+    def __str__(self):
+        return f'{self.name} <{self.time_ms}>'
+
 class Project:
     def __init__(self, json):
         self.name = json["runner"]["run_configuration"]["name"]
         self.refspec = json["runner"]["run_configuration"]["refspec"]
         self.times_ms = json["times-ms"]
 
+        # Collect SMT times
+        self.fn_smt_times = []
+        for item in self.times_ms["smt"]["smt-run-module-times"]:
+            for function in item["function-breakdown"]:
+                self.fn_smt_times.append(FunctionSmtTime(function))
+
     def __str__(self):
-        return f'{self.name} <self.refspec>'
+        return f'{self.name} <{self.refspec}>'
 
 def read_json_files_into_projects(directory):
     projects = []
