9ad7a8310e7e5386913d4de36e7664aa38c17541
Bryan Parno <parno@cmu.edu>
2024-05-17 12:15:10 -0400

    Produce a survival plot

--- scripts/analyze.py
+++ scripts/analyze.py
@@ -3,6 +3,9 @@
 import argparse
 import json
 import glob
+from matplotlib import pyplot as plt
+import numpy as np
+import os
 
 class FunctionSmtTime:
     def __init__(self, json):
@@ -27,6 +30,24 @@ class Project:
     def __str__(self):
         return f'{self.name} <{self.refspec}>'
 
+def plot_project_survival_curve(project):
+    # Calculate survival curve
+    times = [f.time_ms for f in project.fn_smt_times]
+    perf = np.array(np.sort(times))
+    cdf = np.cumsum(perf)
+    plt.plot(cdf, np.arange(0, len(cdf)), label=project.name, linestyle="solid", color="black")
+    plt.legend()
+    plt.ylim(0)
+    plt.xlim(0.1)
+    plt.xscale("log")
+    plt.xlabel("Time Log Scale (ms)")
+    plt.ylabel("Instances Soveld")
+    plt.grid()
+    os.makedirs("fig/survival", exist_ok=True)
+    plt.savefig(f"fig/survival/{project.name}.pdf")
+    plt.close()
+    
+
 def read_json_files_into_projects(directory):
     projects = []
     for filename in glob.glob(f'{directory}/*.json'):
@@ -42,6 +63,7 @@ def main():
     args = parser.parse_args()
 
     projects = read_json_files_into_projects(args.dir)
+    plot_project_survival_curve(projects[0])
 
 if __name__ == '__main__':
     main()
